c23ab0018458c80ea9baf834f419a7bb8f15bcd3
Akshar Patel <akshar.patel.47@gmail.com>
2023-11-26 07:39:32 +0000

    fix middleware again

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,5 +5,5 @@ export default NextAuth(authConfig).auth;
 
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-  matcher: ["/(.*)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)", "/"],
 };
