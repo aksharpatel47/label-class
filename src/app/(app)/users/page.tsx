9fd5febbc296217941bd7dc6fd2aa8a36a2ef65d
JeenalAPatel <jspatel1993@gmail.com>
2023-12-17 19:18:56 +0000

    added user page code

--- src/app/(app)/users/page.tsx
+++ src/app/(app)/users/page.tsx
@@ -1,7 +1,48 @@
-export default function Page() {
+import { H1 } from "@/components/ui/typography";
+import {
+  Table,
+  TableHeader,
+  TableRow,
+  TableHead,
+  TableBody,
+  TableCell,
+} from "@/components/ui/table";
+import { fetchUsers } from "@/lib/data/users";
+import { authUser } from "@/db/schema";
+import Link from "next/link";
+import next from "next";
+
+export default async function Page() {
+  const users = await fetchUsers();
   return (
-    <div>
-      <h1>Users</h1>
-    </div>
+    <>
+      <div className="flex justify-between mb-8">
+        <H1>Users</H1>
+      </div>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Name</TableHead>
+            <TableHead>Email</TableHead>
+            <TableHead>Role</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {users.map((user) => (
+            <TableRow key={user.id}>
+              <TableCell className="font-medium">
+                <Link href={`/users/${user.id}`}>{user.name}</Link>
+              </TableCell>
+              <TableCell className="font-medium">
+                <Link href={`/users/${user.id}`}>{user.id}</Link>
+              </TableCell>
+              <TableCell className="font-medium">
+                <Link href={`/users/${user.id}`}>{user.role}</Link>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </>
   );
 }
