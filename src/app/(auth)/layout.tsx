495834d30de2be9b5b7a655e2878119acdd12a1e
Akshar Patel <akshar.patel.47@gmail.com>
2024-03-09 23:21:58 -0700

    remove signup page temporarily

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,13 +1,3 @@
-import Link from "next/link";
-
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <div>
-      <div className="flex justify-center gap-8 m-8">
-        <Link href="/login">Login</Link>
-        <Link href="/signup">Signup</Link>
-      </div>
-      {children}
-    </div>
-  );
+  return <div className="mt-8">{children}</div>;
 }
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,8 +1,10 @@
 import { redirectIfAuthenticated } from "@/app/lib/utils/session";
 import { SignupForm } from "./form";
+import { redirect } from "next/navigation";
 
 export default async function Page() {
   await redirectIfAuthenticated();
+  redirect("/login");
 
   return (
     <div className="flex justify-center">
