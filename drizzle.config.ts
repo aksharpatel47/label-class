ca41628b6ee8db11bd9884737b355e2de8a9f5c5
Akshar Patel <akshar.patel.47@gmail.com>
2025-06-25 12:21:57 -0700

    update drizzle kit config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,8 +3,9 @@ import type { Config } from "drizzle-kit";
 export default {
   schema: "./src/db/schema.ts",
   out: "./src/db/migrations",
-  driver: "pg", // 'pg' | 'mysql2' | 'better-sqlite' | 'libsql' | 'turso'
+  dialect: "postgresql",
+  driver: "pglite", // 'pg' | 'mysql2' | 'better-sqlite' | 'libsql' | 'turso'
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL!,
   },
 } satisfies Config;
